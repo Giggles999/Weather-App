025b453dbcc64a06fb87bbe8e26d29cbb552bdf1
Lynn M <78457709+Giggles999@users.noreply.github.com>
2021-07-02 16:32:59 -0700

    update condition

--- src/index.js
+++ src/index.js
@@ -104,7 +104,7 @@ function searchLocation(position) {
   let apiKey = "6f65ac3695a44ef64022cd653378b553";
   let units = "imperial"
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}`;
-  axios.get(apiUrl).then(displayWeatherCondition);
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 function getCurrentLocation(event) {
