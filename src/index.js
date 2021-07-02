65d6a7ef31fdce4b766547e1a8066ac0203787d2
Lynn M <78457709+Giggles999@users.noreply.github.com>
2021-07-02 15:42:11 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ function displayWeatherCondition(response) {
   document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
-  document.querySelector("#description").innerHTML = response.data.weather.main;
+  document.querySelector("#description").innerHTML = response.data.weather[0].main;
 
 
   celsiusTemperature = response.data.main.temp;
