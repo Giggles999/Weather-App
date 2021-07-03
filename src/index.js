d066bd814f9b12fecaa328893e13fc6e0ca3f53d
Lynn M <78457709+Giggles999@users.noreply.github.com>
2021-07-02 17:06:15 -0700

    troubleshoot celsius conversion
    not sure why its so high..

--- src/index.js
+++ src/index.js
@@ -102,7 +102,7 @@ function handleSubmit(event) {
 
 function searchLocation(position) {
   let apiKey = "6f65ac3695a44ef64022cd653378b553";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metrics`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
