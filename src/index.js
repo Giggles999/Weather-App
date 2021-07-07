c629f4b950e93948b7b24d775762c97380f45ec0
Lynn M <78457709+Giggles999@users.noreply.github.com>
2021-07-06 21:54:38 -0700

    API Integration
    integrated the API into the forecast template

--- src/index.js
+++ src/index.js
@@ -41,28 +41,45 @@ function formatDate(timestamp) {
 
   return `${day} ${month} ${numberDay} ${year} ${hours}:${minutes}${amOrPm}`;
 }
+//forecast 
+function formatDay(timestamp){
+  let date = new Date(timestamp *1000);
+  let day = date.getDay();
+  let days = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",];
+
+  return days [day];
+}
+
 
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Thur", "Fri", "Sat"];
-  days.forEach(function (day) {
+  
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
     forecastHTML =  forecastHTML +
         `
       <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)} </div>
         <img
-          src="http://openweathermap.org/img/wn/50d@2x.png"
+          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
           alt=""
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span>
-          <span class="weather-forecast-temperature-min"> 27° </span>
+          <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
         </div>
       </div>
-  `;
+  `; }
 });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -72,12 +89,13 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "6f65ac3695a44ef64022cd653378b553";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial}`;
+  let units = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
-
+//display temperature
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temp");
   let cityElement = document.querySelector("#location-city");
@@ -105,8 +123,6 @@ function displayTemperature(response) {
 
 }
 
-
-
 //search city
 function search(city) {
   let apiKey = "6f65ac3695a44ef64022cd653378b553";
@@ -122,7 +138,6 @@ function handleSubmit(event) {
 }
 
 //clicking current button update temp real time base on location
-
 function searchLocation(position) {
   let apiKey = "6f65ac3695a44ef64022cd653378b553";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
